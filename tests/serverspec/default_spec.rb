4e316727a5c7378bf1b8bcc757de1d05a9e93a44
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-08-07 17:22:52 +0900

    [feature] initial implementation (#1)
    * [feature] initial implementation
    
    * update README
    
    * typo

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -2,57 +2,32 @@ require "spec_helper"
 require "serverspec"
 
 package = "emacs"
-service = "emacs"
-config  = "/etc/emacs/emacs.conf"
-user    = "emacs"
-group   = "emacs"
-ports   = [PORTS]
-log_dir = "/var/log/emacs"
-db_dir  = "/var/lib/emacs"
+extra_package = nil
 
 case os[:family]
+when "openbsd"
+  extra_package = "emacs-el"
+when "redhat"
+  package = "emacs-nox"
+  extra_package = "emacs-git-el"
+when "ubuntu"
+  package = "emacs24-nox"
+  extra_package = "emacs-goodies-el"
 when "freebsd"
-  config = "/usr/local/etc/emacs.conf"
-  db_dir = "/var/db/emacs"
+  package = "emacs-nox11"
+  extra_package = "emacs-lisp-intro"
 end
 
 describe package(package) do
   it { should be_installed }
 end
 
-describe file(config) do
-  it { should be_file }
-  its(:content) { should match Regexp.escape("emacs") }
+describe command("emacs --version") do
+  its(:stderr) { should eq "" }
+  its(:stdout) { should match(/^GNU Emacs/) }
+  its(:exit_status) { should eq 0 }
 end
 
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-case os[:family]
-when "freebsd"
-  describe file("/etc/rc.conf.d/emacs") do
-    it { should be_file }
-  end
-end
-
-describe service(service) do
-  it { should be_running }
-  it { should be_enabled }
-end
-
-ports.each do |p|
-  describe port(p) do
-    it { should be_listening }
-  end
+describe package(extra_package) do
+  it { should be_installed }
 end
